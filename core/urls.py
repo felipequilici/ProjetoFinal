fc59b24698e076131f50252a161355fcc7eb819f
Felipe Quilici <felipequilici@gmail.com>
2020-01-03 14:03:54 -0300

    3o commit

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from django.urls import re_path, include
-from .views import home
+from .views import(
+    home,
+    lista_pessoas,
+    lista_veiculos,
+    lista_movrot,
+    lista_mensalistas,
+    lista_movmen,)
+
+
 urlpatterns = [
-    re_path('', home, name='core_home'),
+    re_path('^$', home, name='core_home'),
+    re_path('^pessoas/$', lista_pessoas, name='core_lista_pessoas'),
+    re_path('^veiculos/$', lista_veiculos, name='core_lista_veiculos'),
+    re_path('^movrot/$', lista_movrot, name='core_lista_movrot'),
+    re_path('^mensalistas/$', lista_mensalistas, name='core_lista_movrot'),
+    re_path('^movmen/$', lista_movmen, name='core_lista_movrot'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from core.models import (
+    pessoa,
+    veiculo,
+    movimento_rotativo,
+    mensalistas,
+    MovimentosMensalista)
+
+
 def home(request):
-    return  HttpResponse('OlaMundo')
+    context = {'mensagem':'Olá mundo'}
+    return  render (request, 'core/index.html', context)
+
+
+def lista_pessoas(request):
+    pessoas = pessoa.objects.all()
+    return render(request, 'core/lista_pessoas.html', {'pessoas': pessoas})
+
+def lista_veiculos(request):
+    veiculos = veiculo.objects.all()
+    return render(request, 'core/lista_veiculos.html', {'veiculos': veiculos})
+
+def lista_movrot(request):
+    movrot = movimento_rotativo.objects.all()
+    return render(request, 'core/lista_movrot.html', {'movrot': movrot})
+
+def lista_mensalistas(request):
+    mensalistas = mensalistas.objects.all()
+    return render(request, 'core/lista_movrot.html', {'mensalistas': mensalistas})
+
+def lista_movrot(request):
+    movmen = MovimentosMensalista.objects.all()
+    return render(request, 'core/lista_movrot.html', {'movmen': movmen})
