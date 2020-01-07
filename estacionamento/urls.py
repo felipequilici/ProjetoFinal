b844387bd1fa0b367e93440068ad60cd7d16a287
Felipe Quilici <felipequilici@gmail.com>
2020-01-07 14:06:14 -0300

    BotoesDel

--- estacionamento/urls.py
+++ estacionamento/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import re_path, include
 
 urlpatterns = [
-    re_path('admin/', admin.site.urls),
+    re_path('^admin/', admin.site.urls),
     re_path('core/', include('core.urls')),
 ]
