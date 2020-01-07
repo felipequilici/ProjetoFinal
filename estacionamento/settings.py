03352f3441011d6a75b02ac6823a50232ce29194
Felipe Quilici <felipequilici@gmail.com>
2020-01-07 11:27:12 -0300

    try2

--- estacionamento/settings.py
+++ estacionamento/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','https://estacionamentobalao.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','estacionamentobalao.herokuapp.com','estacionamentobalao.herokuapp.com']
 
 
 
